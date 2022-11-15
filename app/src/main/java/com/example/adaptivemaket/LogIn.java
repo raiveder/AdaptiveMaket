09ff2f9134fb6f78f757d2cc65eef52104422b37
Dmitry <kolovrat-86@yandex.ru>
2022-11-15 22:27:13 +0300

    Messgae

--- app/src/main/java/com/example/adaptivemaket/LogIn.java
+++ app/src/main/java/com/example/adaptivemaket/LogIn.java
@@ -2,7 +2,13 @@ package com.example.adaptivemaket;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LogIn extends AppCompatActivity {
 
@@ -10,5 +16,56 @@ public class LogIn extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        EditText etEmail = findViewById(R.id.etEmail);
+        EditText etPassword = findViewById(R.id.etPassword);
+
+        Button btn = findViewById(R.id.btnLogin);
+
+        etEmail.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable editable) {
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                if (!etEmail.getText().toString().equals("") &&
+                        !etPassword.getText().toString().equals("")) {
+                    btn.setBackground(LogIn.this.getDrawable(R.drawable.button_background_action));
+                }
+                else
+                {
+                    btn.setBackground(LogIn.this.getDrawable(R.drawable.button_background));
+                }
+            }
+        });
+
+        etPassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable editable) {
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                if (!etEmail.getText().toString().equals("") &&
+                        !etPassword.getText().toString().equals("")) {
+                    btn.setBackground(LogIn.this.getDrawable(R.drawable.button_background_action));
+                }
+                else
+                {
+                    btn.setBackground(LogIn.this.getDrawable(R.drawable.button_background));
+                }
+            }
+        });
     }
 }
