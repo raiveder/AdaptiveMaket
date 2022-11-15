3163961ba442dbb13d2571f4f8570156d45a12eb
Сергеев Дмитрий <kolovrat-86@yandex.ru>
2022-11-15 14:52:32 +0300

    Initial commit

---
+++ app/src/main/java/com/example/adaptivemaket/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.adaptivemaket;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(() -> startActivity(new Intent(MainActivity.this, LogIn.class)), 3500);
+    }
+}
